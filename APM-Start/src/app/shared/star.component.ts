39b707c7f6fee1d5cb232745031ad6dff0bd2e64
Ismael Leon <ileon@advansys.com.do>
2023-02-17 07:49:18 -0400

    Prueba

--- APM-Start/src/app/shared/star.component.ts
+++ APM-Start/src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output } from "@angular/core";
 
 @Component({
 selector:'pm-star',
@@ -10,7 +10,16 @@ export class StarComponent implements OnChanges {
     @Input() rating: number =0;
     cropWidth:number=75;
 
+    @Output() ratingClicket: EventEmitter<string>=
+    new EventEmitter<string>();
+
     ngOnChanges():void{
         this.cropWidth = this.rating * 75/5;
     }
+
+    onClick(): void{
+        console.log(`The rating ${this.rating} was clicked`);
+    }
+
+
 }
