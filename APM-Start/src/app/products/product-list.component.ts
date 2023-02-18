b73c3eb80c904c5d9e712c70fd5b775e33557d73
ISMAEL <ingismaelleon@hotmail.com>
2023-02-18 17:49:19 -0400

    Hasta el modulo 11

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subscription } from "rxjs";
 import { Iproduct } from "./product";
 import { ProductService } from "./product.service";
 
 @Component({
-    selector: 'pm-products',
     templateUrl: './product-list.component.html',
     styleUrls:['./product-list.component.css']
 })
-export class  ProductListComponent implements OnInit {
+export class  ProductListComponent implements OnInit, OnDestroy {
 pageTitle: string='Product List!';
 imageWidth: number = 50;
 imageMargin: number = 2;
 showImage: boolean = false;
+errorMessage: string = '';
+sub!: Subscription;
 
 private _listFilter: string= '';
 get listFilter(): string{
@@ -39,8 +41,18 @@ this.showImage= !this.showImage;
 }
 
 ngOnInit(): void {
-this.products = this.productService.getProducts();
-this.filteredProducts = this.products;
+this.sub = this.productService.getProducts().subscribe({
+  next: products => {
+    this.products = products;
+    this.filteredProducts = this.products;
+  },
+  error: err =>  this.errorMessage = err
+});
+
+}
+
+ngOnDestroy(){
+  this.sub.unsubscribe(); 
 }
 
 onRatingClicked(message: string): void{
